6d53f4f603059d1b87e9cd0653fe9955bab19926
kerim_ozben <kerim.ozben@consultant.morhipo.com>
2022-03-16 15:00:15 +0300

    Add project files.

---
+++ Encryption-Decryption/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+
+byte[] passwordHash, passwordKey;
+Cryptography c = new();
+
+string password = "kerim123";
+
+c.PasswordEncrypt(password, out passwordHash, out passwordKey);
+
+Console.WriteLine(password);
+Console.WriteLine(Encoding.UTF8.GetString(passwordKey));
+Console.WriteLine(Encoding.UTF8.GetString(passwordHash));
+
+var result = c.VeriyfPassword(password,passwordHash,passwordKey);
+Console.WriteLine(result);
+
+Console.ReadLine();
+
+public class Cryptography
+{
+    public void PasswordEncrypt(string password, out byte[] passwordHash, out byte[] passwordKey)
+    {
+        using (var hmac = new HMACSHA512())
+        {
+            passwordKey = hmac.Key;
+            passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+        }
+    }
+
+    public bool VeriyfPassword(string password, byte[] passwordHash, byte[] passwordKey)
+    {
+        using (var hmac = new HMACSHA512(passwordKey))
+        {
+            var hash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password));
+
+            for (int i = 0; i < passwordHash.Length; i++)
+            {
+                if (hash[i] != passwordHash[i])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
